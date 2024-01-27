7fb74fb3b10cf6ec2d012b737f2f601fa5246bd9
theryeddari <121760376+theryeddari@users.noreply.github.com>
2024-01-27 17:19:42 +0100

    Initial commit

---
+++ src/main/java/com/safetynet1/app/Safetynet1Application.java
@@ -0,0 +1,13 @@
+package com.safetynet1.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Safetynet1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Safetynet1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetynet1/app/Safetynet1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet1.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Safetynet1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
